2cac97c93242e6421bc5633b4616fb6bcfdaa95e
Nandir <nandir.be@gmail.com>
2020-10-02 19:44:20 +0800

    project 2 oruulah

---
+++ lab2/project2/cs142-make-multi-filter.js
@@ -0,0 +1,8 @@
+function cs142MakeMultiFilter(arr) {
+  const originalArray = arr;
+  const currentArray = arr;
+
+  const filteredArray= arrayFilterer(filterCriteria, callback)
+
+  return filteredArray;
+}
